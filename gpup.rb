841dacde1561a95b63d00460bbd7b71ec9186793
goreleaserbot <goreleaser@carlosbecker.com>
2018-10-02 16:20:54 +0900

    Brew formula update for gpup version 1.6

--- gpup.rb
+++ gpup.rb
@@ -1,9 +1,9 @@
 class Gpup < Formula
   desc "Upload files to your Google Photos with the new Photos Library API"
   homepage "https://github.com/int128/gpup"
-  url "https://github.com/int128/gpup/releases/download/1.5/gpup_1.5_darwin_amd64.tar.gz"
-  version "1.5"
-  sha256 "04fe39143d3c10b522015517de6252caff436000aca95df591940db304cde0b4"
+  url "https://github.com/int128/gpup/releases/download/1.6/gpup_1.6_darwin_amd64.tar.gz"
+  version "1.6"
+  sha256 "fa15b8e1df8ed040f13c511dfab3a68c741005c2056f609d84321c2f767f9071"
 
   def install
     bin.install "gpup"
